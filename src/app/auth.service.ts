aa1397f92f42446ae3e4654e01380421aaef7026
giojgerenaia <giorgi.jgerenaia.2@iliauni.edu.ge>
2024-08-23 20:37:58 +0400

    added

---
+++ src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor() {}
+
+  saveLoginData(loginData: any) {
+    localStorage.setItem('loginData', JSON.stringify(loginData));
+  }
+
+  getLoginData(): any {
+    const data = localStorage.getItem('loginData');
+    return data ? JSON.parse(data) : null;
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../auth.service';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router : Router) {}
+
+  onLogin(): void {
+    if (this.username && this.password) {
+      const loginData = { username: this.username, password: this.password };
+
+      this.authService.saveLoginData(loginData);
+      this.router.navigate(['/main'])
+
+
+    }
+  }
+}
