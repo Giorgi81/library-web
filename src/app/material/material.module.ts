0d9ef4d0f041653e3cbc5aa10f34995d89ba8aef
giojgerenaia <giorgi.jgerenaia.2@iliauni.edu.ge>
2024-08-25 05:10:59 +0400

    added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,30 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatCardModule} from "@angular/material/card";
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatIcon, MatIconModule} from '@angular/material/icon';
-
-
-
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  exports : [
+  exports: [
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
     MatCardModule,
     MatDatepickerModule,
     MatIconModule
-
   ],
+
   declarations: [],
   imports: [
-    CommonModule,
-
+    CommonModule
   ]
 })
-export class MaterialModule { }
+
+export class MaterialModule {
+}
